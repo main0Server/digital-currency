9d5c0c823c0d8c0b5132c341d73ca277d517ed8a
dell <mainserver90@gmail.com>
2022-03-13 00:17:05 +0100

    import provider. import store. Every single component inside of the app is going to have access to the store variable!

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+
+// Provider
+import { Provider } from "react-redux";
+// Store (Is a variable to provide to the Provider)
+import store from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
