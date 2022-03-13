ada6e4ae553eba3f731068648cda716bb6ff9ff9
dell <mainserver90@gmail.com>
2022-03-13 19:50:42 +0100

    add margin-top

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   const copyRight = new Date();
   return (
-    <div className="bg-dark text-light text-center rounded p-5">
+    <div className="bg-dark text-light text-center rounded mt-4 p-5">
       Copyright &copy; {copyRight.getFullYear()}
     </div>
   );
