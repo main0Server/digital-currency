885ff766b5347e3422a62e25484b3e63e4a57b95
dell <mainserver90@gmail.com>
2022-03-13 16:50:11 +0100

    update Loader style

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,7 @@
 const Loader = () => {
   return (
-    <div className="m-2 text-center">
-      <div className="spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
+    <div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
     </div>
   );
 };
