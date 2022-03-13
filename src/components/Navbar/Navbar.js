8903406ee2896b9bbee0be310f410d37eacacaf5
dell <mainserver90@gmail.com>
2022-03-13 12:16:30 +0100

    delete nav button & search input

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,34 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="REACT_Navbar">
-      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow mb-4">
         <NavLink className="navbar-brand col-md-3 col-lg-2 me-0 px-3" to="/">
           Cryptocurrencies
         </NavLink>
-        <button
-          className="navbar-toggler position-absolute d-md-none collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#sidebarMenu"
-          aria-controls="sidebarMenu"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <input
-          className="form-control form-control-dark w-100"
-          type="text"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <div className="navbar-nav">
-          <div className="nav-item text-nowrap">
-            <a className="nav-link px-3" href="/">
-              Sign out
-            </a>
-          </div>
-        </div>
       </header>
 
       <div className="container-fluid">
