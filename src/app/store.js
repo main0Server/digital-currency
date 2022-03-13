0195ca0210309bcfb51ce1f32179512d3faed23d
dell <mainserver90@gmail.com>
2022-03-13 12:11:56 +0100

    add middleware && listeners reduxjs/toolkit/query

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,18 @@
 // Store is on central state of throat
 import { configureStore } from "@reduxjs/toolkit";
 
+import { setupListeners } from "@reduxjs/toolkit/query";
+
 // Api
 import { cryptoApi } from "../services/cryptoApi";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
+
+setupListeners(configureStore.dispatch)
